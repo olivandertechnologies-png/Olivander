e0fb1fc529ba1479951ca01741f05a4d2d8eca62
Ollie Blakey <olivandertechnologies@gmail.com>
2026-03-24 10:32:37 +1300

    Remove wand branding and simplify wordmark

--- olivander-proto/frontend/src/main.jsx
+++ olivander-proto/frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { WandProvider } from './components/OlivanderWand';
 import './styles/tokens.css';
 import './styles/global.css';
 import './styles/dashboard.css';
@@ -11,8 +10,6 @@ document.documentElement.classList.toggle('dark', saved === 'dark');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WandProvider>
-      <App />
-    </WandProvider>
+    <App />
   </React.StrictMode>,
 );
